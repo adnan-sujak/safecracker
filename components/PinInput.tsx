6e5043f46c166c8376035a11f4689d7ca2f2f8d7
adnan-sujak <sujakadnan@gmail.com>
2026-02-02 21:14:27 -0500

    Added logic to swap out placeholder with actual pin pad number

--- components/PinInput.tsx
+++ components/PinInput.tsx
@@ -1,63 +1,50 @@
 import React from "react";
 import { useState } from "react";
-import { ChevronDown, ChevronUp } from "lucide-react";
+
 
 export default function PinInput() {
-  //   const [pinNumber, setPinNumber] = useState("");
-  //   const [counter, setCounter] = useState(0);
 
-  // const pin: any = [];
+  function generatePin() {
+    return Math.floor(Math.random() * 900) + 100;
+  }
 
-  //   function handlePinChange(number) {
-  //     setPinNumber(number.target.value);
-  //   }
-
-  //   const incrementCounter = () => {
-  //     setCounter(counter + 1);
-  //   };
-
-  //   const decrementCounter = () => {
-  //     if (counter !== 0) {
-  //       setCounter(counter - 1);
-  //     }
-  //   };
-
-
-
-  // for (let i = 1; i <= 9; i++) {
-  //   pin.push(<button key={i}>{i}</button>)
-  // }
-
-
-  const [guess, setGuess] = useState<number[]>([])
+  const PIN_LEN = 3;
+  const [guess, setGuess] = useState<number[]>([]);
+  const [pin, setPin] = useState(() => generatePin());
+  
 
   return (
     <div className="grid grid-cols-2 place-items-center h-125 w-250 bg-gray-600 rounded-4xl ">
-      <div className="flex justify-between">
-        <p className="text-4xl font-extrabold text-black">*</p>
-        <p className="text-4xl font-extrabold text-black">*</p>
-        <p className="text-4xl font-extrabold text-black">*</p>
-
-        {guess}
+      <div className="flex gap-4">
+        {Array.from({ length: PIN_LEN }).map((_, i) => (
+          <p
+            key={i}
+            className="text-4xl font-extrabold text-black w-8 text-center"
+          >
+            {guess[i] !== undefined ? guess[i] : "*"}
+          </p>
+        ))}
       </div>
 
-      <div className="grid grid-cols-3 grid-rows-4 aspect-3/4 h-full bg-red-300">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map(key =>
+      <div className="grid grid-cols-3 grid-rows-4 aspect-3/4 h-115 rounded-3xl bg-red-300">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((key) => (
           <button
             key={key}
-            onClick={() => {
-              setGuess(guess => [...guess, key])
-            }}
             className={key === 0 ? "col-span-2" : undefined}
+            onClick={() => {
+              setGuess((prev) =>
+                prev.length >= PIN_LEN ? prev : [...prev, key],
+              );
+            }}
           >
             {key}
           </button>
-        )}
-        <button onClick={() => {
-          setGuess(last => last.slice(0, -1))
-        }}> del </button>
-      </div>
+        ))}
 
+        <button onClick={() => setGuess((prev) => prev.slice(0, -1))}>
+          del
+        </button>
+      </div>
     </div>
   );
 }
