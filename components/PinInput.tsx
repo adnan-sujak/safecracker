02a7f94c5ac48a63282e5724be65d175ab96ddb1
adnan-sujak <sujakadnan@gmail.com>
2026-02-02 19:54:27 -0500

    added pin pad

--- components/PinInput.tsx
+++ components/PinInput.tsx
@@ -3,34 +3,61 @@ import { useState } from "react";
 import { ChevronDown, ChevronUp } from "lucide-react";
 
 export default function PinInput() {
-//   const [pinNumber, setPinNumber] = useState("");
-//   const [counter, setCounter] = useState(0);
+  //   const [pinNumber, setPinNumber] = useState("");
+  //   const [counter, setCounter] = useState(0);
 
-//   const pin: number[] = [];
+  // const pin: any = [];
 
-//   function handlePinChange(number) {
-//     setPinNumber(number.target.value);
-//   }
+  //   function handlePinChange(number) {
+  //     setPinNumber(number.target.value);
+  //   }
 
-//   const incrementCounter = () => {
-//     setCounter(counter + 1);
-//   };
+  //   const incrementCounter = () => {
+  //     setCounter(counter + 1);
+  //   };
 
-//   const decrementCounter = () => {
-//     if (counter !== 0) {
-//       setCounter(counter - 1);
-//     }
-//   };
+  //   const decrementCounter = () => {
+  //     if (counter !== 0) {
+  //       setCounter(counter - 1);
+  //     }
+  //   };
+
+
+
+  // for (let i = 1; i <= 9; i++) {
+  //   pin.push(<button key={i}>{i}</button>)
+  // }
+
+
+  const [guess, setGuess] = useState<number[]>([])
 
   return (
-    <div className="flex flex-col items-center justify-center h-125 w-250 bg-gray-300 rounded-4xl ">
-      <ChevronUp />
-      <div className="flex flex-row justify-between">
-        <p className="text-4xl font-extrabold">*</p>
-        <p className="text-4xl font-extrabold">*</p>
-        <p className="text-4xl font-extrabold">*</p>
+    <div className="grid grid-cols-2 place-items-center h-125 w-250 bg-gray-600 rounded-4xl ">
+      <div className="flex justify-between">
+        <p className="text-4xl font-extrabold text-black">*</p>
+        <p className="text-4xl font-extrabold text-black">*</p>
+        <p className="text-4xl font-extrabold text-black">*</p>
+
+        {guess}
       </div>
-      <ChevronDown />
+
+      <div className="grid grid-cols-3 grid-rows-4 aspect-3/4 h-full bg-red-300">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map(key =>
+          <button
+            key={key}
+            onClick={() => {
+              setGuess(guess => [...guess, key])
+            }}
+            className={key === 0 ? "col-span-2" : undefined}
+          >
+            {key}
+          </button>
+        )}
+        <button onClick={() => {
+          setGuess(last => last.slice(0, -1))
+        }}> del </button>
+      </div>
+
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,14 +2,14 @@ import PinInput from "../../components/PinInput";
 
 function Home() {
   return (
-    <div className="relative flex flex-col items-center justify-center bg-gray-100 h-screen w-screen">
+    <div className="relative flex flex-col items-center justify-center bg-white h-screen w-screen">
       <div className="flex flex-row items-center justify-between w-full px-4 -mt-12 mb-15">
-        <p className="font-bold"> $0 </p>
+        <p className="font-bold text-black"> $0 </p>
 
-        <h1> Guess the pin! </h1>
+        <h1 className="text-black"> Guess the pin! </h1>
 
         <div className="flex flex-col items-center justify-center h-20 w-20 bg-cyan-400 rounded-3xl">
-          <p className="text-center"> Inventory </p>
+          <p className="text-center text-black"> Inventory </p>
         </div>
       </div>
 
